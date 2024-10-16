fb4914ef163faf8ca56bf48f1a84040fa3e2ad6b
Kyle Nazario <nazariosoftwarellc@gmail.com>
2024-10-15 22:05:50 -0600

    Removes pregenerated code

--- Extension/scripts/build.js
+++ Extension/scripts/build.js
@@ -1,6 +1,4 @@
 const { build } = require('esbuild');
-const sveltePlugin = require('esbuild-svelte');
-const sveltePreprocess = require('svelte-preprocess');
 
 const isProdBuild = process.argv.includes('--prod');
 
@@ -23,39 +21,7 @@ async function main() {
     outfile: './dist/content.js'
   });
 
-  const backgroundJob = build({
-    ...commonConfig,
-    entryPoints: ['./src/background.ts'],
-    outfile: './dist/background.js'
-  });
-
-  const popupJob = build({
-    ...commonConfig,
-    entryPoints: ['./src/popup/popup.ts'],
-    outbase: './src/popup',
-    outdir: './dist',
-    mainFields: ['svelte', 'module', 'main', 'browser'],
-    plugins: [
-      sveltePlugin({
-        preprocess: sveltePreprocess()
-      })
-    ]
-  });
-
-  const settingsJob = build({
-    ...commonConfig,
-    entryPoints: ['./src/settings/settings.ts'],
-    outbase: './src/settings',
-    outdir: './dist',
-    mainFields: ['svelte', 'module', 'main', 'browser'],
-    plugins: [
-      sveltePlugin({
-        preprocess: sveltePreprocess()
-      })
-    ]
-  });
-
-  return Promise.all([contentJob, backgroundJob, popupJob, settingsJob]).then(
+  return Promise.all([contentJob]).then(
     () => console.log('⚡ Compiled')
   );
 }
